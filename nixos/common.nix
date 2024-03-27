ecbcd1e1c8be54507de3645e23c719b9eda5d67b
curtkim <iamteri@gmail.com>
2024-03-27 11:56:32 +0900

    kime설정

--- nixos/common.nix
+++ nixos/common.nix
@@ -30,7 +30,32 @@
     LC_TIME = "ko_KR.UTF-8";
   };
   i18n.inputMethod.enabled = "kime";
+  i18n.inputMethod.kime.daemonModules = ["Indicator"]; # "Wayland" 
   i18n.inputMethod.kime.iconColor = "White";
+  i18n.inputMethod.kime.extraConfig = ''
+  log:
+    global_level: DEBUG
+  engine:
+    translation_layer: null
+    default_category: Latin
+    global_category_state: true
+    global_hotkeys:
+      C-Space:
+        behavior: !Toggle
+        - Hangul
+        - Latin
+        result: Consume
+      AltR:
+        behavior: !Toggle
+        - Hangul
+        - Latin
+        result: Consume
+      Hangul:
+        behavior: !Toggle
+        - Hangul
+        - Latin
+        result: Consume
+  '';
 #  i18n.inputMethod = {
 #    enabled = "fcitx5";
 #    fcitx5.addons = with pkgs; [
@@ -46,6 +71,8 @@
       nanum
       nanum-gothic-coding
       (nerdfonts.override { fonts = ["JetBrainsMono" "Hack"]; })
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
     ];
   
     fontconfig = {
