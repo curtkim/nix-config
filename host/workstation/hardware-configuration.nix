3f279dd635da2f1cc5cea41bdd5c8656474cbc05
curtkim <iamteri@gmail.com>
2024-04-11 06:49:33 +0900

    add wifi driver

--- host/workstation/hardware-configuration.nix
+++ host/workstation/hardware-configuration.nix
@@ -10,8 +10,9 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+
+  boot.kernelModules = ["88x2bu"];
+  boot.extraModulePackages = [ config.boot.kernelPackages.rtl88x2bu ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
