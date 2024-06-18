c51cc1fe52c34d5ae13204fca3759aead0e07faf
curtkim <iamteri@gmail.com>
2024-06-18 17:24:54 +0900

    add obsidian

--- user/default.nix
+++ user/default.nix
@@ -95,6 +95,8 @@
     # gnome
     gnome.gnome-session
 
+    obsidian
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
