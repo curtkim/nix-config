256facc80ee2fe28511819a65b89389eb265834c
curtkim <iamteri@gmail.com>
2024-06-19 15:27:42 +0900

    change nix.settings

--- host/common.nix
+++ host/common.nix
@@ -1,7 +1,20 @@
 { config, pkgs, ... }:
 {
   nix = {
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
 
   nixpkgs.config.allowUnfree = true;
