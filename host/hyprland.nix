341baafe72a044099f74b2abea6ccc156bc4dd64
curtkim <iamteri@gmail.com>
2024-08-11 15:17:42 +0900

    NIXOS_OZONE_WL 추가

--- host/hyprland.nix
+++ host/hyprland.nix
@@ -20,6 +20,12 @@
       xwayland.enable = true;
   };
 
+  environment = {
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+  };
+
   #  programs.hyprland = {
   #    enable = true;
   #    package = hyprland.packages."${pkgs.system}".hyprland;
