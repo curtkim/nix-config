9d14be864e12dc1fe6fc2ed38d285a4b44f8d3b5
curtkim <iamteri@gmail.com>
2024-09-15 11:41:17 +0900

    inkscape

--- user/default.nix
+++ user/default.nix
@@ -130,6 +130,8 @@
     # dap
     gdb
 
+    inkscape
+
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
