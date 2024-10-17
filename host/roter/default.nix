5ffbdc6624ae693d4e22dd3d67be900c018988bb
curtkim <iamteri@gmail.com>
2024-10-17 16:24:17 +0900

    -m

---
+++ host/roter/default.nix
@@ -0,0 +1,22 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ xconfig, pkgs, disko, hostName, ... }:
+
+{
+  imports =
+    [
+      ../boot.nix
+      disko.nixosModules.disko
+      ./disko-config.nix
+      ./hardware-configuration.nix
+      #../amd.nix
+      ../common.nix
+      #../xserver.nix
+      #../hyprland.nix
+      #../printing.nix
+    ];
+
+  networking.hostName = hostName; # Define your hostname.
+}
