db9bb0e4afbea60fa01e1de40473c576edd310e0
curtkim <iamteri@gmail.com>
2024-11-19 16:20:59 +0900

    disable ssh IdentityAgent yubikey-agent

--- user/ssh.nix
+++ user/ssh.nix
@@ -1,17 +1,17 @@
 {config, ...}: {
   programs.ssh = {
     enable = true;
-    extraConfig = ''
-      Host 192.168.0.147
-        IdentityAgent /run/user/1000/yubikey-agent/yubikey-agent.sock
-    '';
+    #    extraConfig = ''
+    #      Host 192.168.0.147
+    #        IdentityAgent /run/user/1000/yubikey-agent/yubikey-agent.sock
+    #    '';
 
-    matchBlocks = {
-      "192.168.0.147" = {
-        extraOptions = {
-          IdentityAgent = "/run/user/1000/yubikey-agent/yubikey-agent.sock";
-        };
-      };
-    };
+    #    matchBlocks = {
+    #      "192.168.0.147" = {
+    #        extraOptions = {
+    #          IdentityAgent = "/run/user/1000/yubikey-agent/yubikey-agent.sock";
+    #        };
+    #      };
+    #    };
   };
 }
