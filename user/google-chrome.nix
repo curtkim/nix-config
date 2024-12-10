dd0f0e1c198fcfc17f4e203079bac3b12aa0f9a1
curtkim <iamteri@gmail.com>
2024-12-10 23:41:33 +0900

    google-chrome, nvim을 pkgs에서 가져오도록 수정. nixpkgs-unstable을 update

--- user/google-chrome.nix
+++ user/google-chrome.nix
@@ -3,11 +3,13 @@
 {
   programs.google-chrome = {
     enable = true;
+
     # ver < 125 에서 --enable-wayland-ime관련 문제가 있었다.
-    package = pkgs-unstable.google-chrome;
+    #package = pkgs-unstable.google-chrome;
+    package = pkgs.google-chrome;
 
     commandLineArgs = [
-      "--ozone-platform=wayland"
+      #"--ozone-platform=wayland"
       "--enable-wayland-ime"
     ];
   };
