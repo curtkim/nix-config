d411bac557f28eaf942f1b7ff936f8554888a21a
curtkim <iamteri@gmail.com>
2024-12-19 09:43:02 +0900

    cmatrix 추가

--- user/default.nix
+++ user/default.nix
@@ -56,6 +56,9 @@
     pamixer
 
 
+    # joy
+    cmatrix
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
