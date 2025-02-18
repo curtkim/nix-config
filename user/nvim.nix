371dc25f41605313719125e24a671fb6ff606f3f
curtkim <iamteri@gmail.com>
2025-02-18 12:33:17 +0900

    nvim add luarocks for image.nvim

--- user/nvim.nix
+++ user/nvim.nix
@@ -8,6 +8,13 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+
+    extraLuaPackages = ls:
+      with ls; [
+        luarocks
+        # required by 3rd/image.nvim
+        magick
+      ];
   };
 }
 
