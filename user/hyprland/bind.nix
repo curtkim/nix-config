c7175c4ca9bf5e27342ebad42c84f8a4e95314bd
curtkim <iamteri@gmail.com>
2025-03-12 14:24:01 +0900

    hyprland bind. mainMod 0, resizeactive exact 85% 15%

--- user/hyprland/bind.nix
+++ user/hyprland/bind.nix
@@ -28,15 +28,12 @@ _: {
       "$mainMod, 3, workspace, 3"
       "$mainMod, 4, workspace, 4"
       "$mainMod, 5, workspace, 5"
-      "$mainMod, 6, workspace, 6"
 
+      "$mainMod, 6, resizeactive, exact 15% 85%"
       "$mainMod, 7, resizeactive, exact 25% 75%"
       "$mainMod, 8, resizeactive, exact 50% 50%"
       "$mainMod, 9, resizeactive, exact 75% 25%"
-      #"$mainMod, 7, workspace, 7"
-      #"$mainMod, 8, workspace, 8"
-      #"$mainMod, 9, workspace, 9"
-      #"$mainMod, 0, workspace, 10"
+      "$mainMod, 0, resizeactive, exact 85% 15%"
 
       # Move active window to a workspace with mainMod + SHIFT + [0-9]
       "$mainMod SHIFT, 1, movetoworkspace, 1"
