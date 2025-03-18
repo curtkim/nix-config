2c86a9136190b4e58a2f7d4cce09ff96949f72ba
curtkim <iamteri@gmail.com>
2025-03-18 19:40:35 +0900

    minor

--- user/chromium.nix
+++ user/chromium.nix
@@ -1,24 +1,24 @@
 { pkgs, pkgs-unstable, ... }:
 
 {
-  programs.chromium = {
-    enable = true;
-    # ver < 125 에서 --enable-wayland-ime관련 문제가 있었다.
-    package = pkgs-unstable.ungoogled-chromium;
-
-    commandLineArgs = [
-      "--ozone-platform=wayland"
-      "--enable-wayland-ime"
-      "--no-default-browser-check"
-      "--restore-last-sesion"
-    ];
-    extensions = [
-      # vimium C
-      "hfjbmagddngcpeloejdejnfgbamkjaeg"
-      "nacjakoppgmdcpemlfnfegmlhipddanj"
-
-      # mouse tooltip translator
-      "hmigninkgibhdckiaphhmbgcghochdjc"
-    ];
-  };
+#  programs.chromium = {
+#    enable = true;
+#    # ver < 125 에서 --enable-wayland-ime관련 문제가 있었다.
+#    package = pkgs-unstable.ungoogled-chromium;
+#
+#    commandLineArgs = [
+#      "--ozone-platform=wayland"
+#      "--enable-wayland-ime"
+#      "--no-default-browser-check"
+#      "--restore-last-sesion"
+#    ];
+#    extensions = [
+#      # vimium C
+#      "hfjbmagddngcpeloejdejnfgbamkjaeg"
+#      "nacjakoppgmdcpemlfnfegmlhipddanj"
+#
+#      # mouse tooltip translator
+#      "hmigninkgibhdckiaphhmbgcghochdjc"
+#    ];
+#  };
 }
